8b3273e6e7f9b980a5b6878d94bf895fd604767f
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-02-28 11:43:07 +0100

    Update download.go

--- api/download.go
+++ api/download.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"api-gateway/img"
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -10,7 +11,7 @@ func ImageDownloadHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO : SUPPRIMER POUR PROD
 	// Set CORS headers
 	w.Header().Set("Access-Control-Allow-Origin", "*") // or specify your domain
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 	if r.Method == "GET" {
@@ -21,7 +22,7 @@ func ImageDownloadHandler(w http.ResponseWriter, r *http.Request) {
 			// Appel de l'API saveIMG
 			imgData, err := img.GetImageFromId(imageId)
 			if err != nil {
-				http.Error(w, "Erreur lors de l'appel de l'API saveIMG", http.StatusInternalServerError)
+				http.Error(w, fmt.Sprintf("Erreur lors de l'appel de l'API saveIMG : %s",err), http.StatusInternalServerError)
 				return
 			}
 
