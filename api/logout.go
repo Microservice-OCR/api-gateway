1170496fd5abcb8844218041c9ec68088de26678
Matteo <mrodrigues7@myges.fr>
2024-03-04 21:29:33 +0100

    add logout func

---
+++ api/logout.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"bytes"
+	"net/http"
+)
+
+func GatewayLogoutHandler(w http.ResponseWriter, r *http.Request) {
+	tokenHeader := r.Header.Get("Authorization")
+	if tokenHeader == "" {
+		http.Error(w, "No token provided", http.StatusBadRequest)
+		return
+	}
+
+	authServiceURL := "https://auth-theta-opal.vercel.app/api/logout"
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", authServiceURL, bytes.NewBufferString(""))
+	if err != nil {
+		http.Error(w, "Failed to create request to auth service", http.StatusInternalServerError)
+		return
+	}
+	req.Header.Add("Authorization", tokenHeader)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, "Failed to request auth service", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		http.Error(w, "Failed to logout", http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "https://cloudocr.vercel.app/", http.StatusSeeOther)
+}
