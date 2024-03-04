2f34dfcd66ed9c6fa62d571a0d3fecf5f206ab87
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-03-04 10:11:19 +0100

    bypass middleware

--- middleware/jwtMiddleware.go
+++ middleware/jwtMiddleware.go
@@ -2,7 +2,7 @@ package middleware
 
 import (
 	"net/http"
-	"strings"
+	// "strings"
 	"time"
 )
 
@@ -17,35 +17,35 @@ func JwtMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// exécuter le gestionnaire suivant
 		next(w, r)
-		authHeader := r.Header.Get("Authorization")
-		if authHeader == "" {
-			// rediriger vers /login frontend
-			http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusUnauthorized)
-			return
-		}
-		parts := strings.Split(authHeader, " ")
-		if len(parts) != 2 || parts[0] != "Bearer" {
-			// rediriger vers /login frontend
-			http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusUnauthorized)
-			return
-		}
-		jwtToken := parts[1]
+		// authHeader := r.Header.Get("Authorization")
+		// if authHeader == "" {
+		// 	// rediriger vers /login frontend
+		// 	http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusUnauthorized)
+		// 	return
+		// }
+		// parts := strings.Split(authHeader, " ")
+		// if len(parts) != 2 || parts[0] != "Bearer" {
+		// 	// rediriger vers /login frontend
+		// 	http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusUnauthorized)
+		// 	return
+		// }
+		// jwtToken := parts[1]
 
-		// Vérifie si le JWT existe dans sessions
-		session, exists := sessions[jwtToken]
-		if !exists {
-			// rediriger vers /login frontend
-			http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusUnauthorized)
-			return
-		}
+		// // Vérifie si le JWT existe dans sessions
+		// session, exists := sessions[jwtToken]
+		// if !exists {
+		// 	// rediriger vers /login frontend
+		// 	http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusUnauthorized)
+		// 	return
+		// }
 
-		// Vérifie si plus de 10 minutes se sont écoulées depuis la connexion
-		if time.Since(session.ConnectedAt) > 10*time.Minute {
-			// modifier l'url pour mettre l'url du frontend
-			// Si le temps actuel est égale au temps enregistrer dans la session
-			http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusUnauthorized)
-			return
-		}
+		// // Vérifie si plus de 10 minutes se sont écoulées depuis la connexion
+		// if time.Since(session.ConnectedAt) > 10*time.Minute {
+		// 	// modifier l'url pour mettre l'url du frontend
+		// 	// Si le temps actuel est égale au temps enregistrer dans la session
+		// 	http.Error(w, "Erreur lors de la lecture de la réponse", http.StatusUnauthorized)
+		// 	return
+		// }
 
 		
 	}
