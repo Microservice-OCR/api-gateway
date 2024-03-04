e4b986ef08a8cbbce644e558f7ee87953f89a1ad
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-03-04 23:06:28 +0100

    update CORS

--- middleware/jwtMiddleware.go
+++ middleware/jwtMiddleware.go
@@ -8,10 +8,6 @@ import (
 
 func JwtMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-        // Votre logique de gestion d'upload
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		tokenHeader := r.Header.Get("Authorization")
 		if tokenHeader == "" {
 			http.Error(w, "Authorization header is missing", http.StatusUnauthorized)
