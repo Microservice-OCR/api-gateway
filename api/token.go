c1c83488cc8ab54bc4bf107f57507512d2a6dc88
Matteo <mrodrigues7@myges.fr>
2024-03-04 22:04:44 +0100

    correct token.go

--- api/token.go
+++ api/token.go
@@ -5,15 +5,9 @@ import (
 	"io/ioutil"
 )
 
-// TokenResponse représente la structure de la réponse attendue contenant les tokens
-// type TokenResponse struct {
-// 	Tokens []models.Output `json:"tokens"`
-// }
-
 func GetAllTokensFromAuthService(w http.ResponseWriter, r *http.Request) {
 	authServiceURL := "https://auth-theta-opal.vercel.app/api/token"
 
-	// Création de la requête HTTP pour appeler le service d'authentification
 	resp, err := http.Get(authServiceURL)
 	if err != nil {
 		http.Error(w, "Failed to connect to auth service", http.StatusInternalServerError)
@@ -27,16 +21,6 @@ func GetAllTokensFromAuthService(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Décommentez et ajustez en fonction de la structure de réponse de votre service d'authentification
-	// var tokenResponse TokenResponse
-	// err = json.Unmarshal(body, &tokenResponse)
-	// if err != nil {
-	//     http.Error(w, "Error parsing JSON response from auth service", http.StatusInternalServerError)
-	//     return
-	// }
-
-	// Réponse au client avec les tokens récupérés
-	// Utilisez `tokenResponse` comme corps de la réponse si vous utilisez la structure de décodage JSON ci-dessus
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(body) // Ou `jsonData` après le décodage et le re-codage en JSON si nécessaire
+	w.Write(body) 
 }
