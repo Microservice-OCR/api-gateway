f236173af2d1db82e25e56204a869d881753e701
Matteo <mrodrigues7@myges.fr>
2024-03-05 03:41:18 +0100

    CORS route

--- api/signup.go
+++ api/signup.go
@@ -12,23 +12,12 @@ type SignupRequest struct {
 	Password string `json:"password"`
 }
 
-func setupCORS(w *http.ResponseWriter, req *http.Request) {
-    (*w).Header().Set("Access-Control-Allow-Origin", "https://cloudocr.vercel.app")
-    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-}
-
 func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO : SUPPRIMER POUR PROD
 	// Set CORS headers
 	w.Header().Set("Access-Control-Allow-Origin", "*") 
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	setupCORS(&w, r)
-    if r.Method == "OPTIONS" {
-        return
-    }
-
 
 	if r.Method == "OPTIONS" {
         w.WriteHeader(http.StatusOK)
